b03644c60b250476b2c07feca38ab2a5a0e6f52a
Andrey Samsonkin <newwrench@gmail.com>
2020-06-01 14:13:30 +0300

    Create 7.pl

---
+++ 7.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+my %countries = qw(England English
+    France French
+    Spain Spanish
+    China Chinese
+    Germany German);
+print("".%countries);
