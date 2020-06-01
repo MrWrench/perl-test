b6e2f9cef8190b0ca8a97aea66a2a9e6ce3d6146
Andrey Samsonkin <newwrench@gmail.com>
2020-06-01 13:48:18 +0300

    Create 6.pl

---
+++ 6.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+my $str = 'sad 123 dsfsdagjsdfgh345fgvdkljgsd 98 t 347 985fjkldsfjsdklf 8 9437 598 3247 291 3718 97dsgfadkif349 875jgdfkl;jb90485tyjgdsof
+gtr698409ugoldfkhg95683740';
+my @list = ($str =~ m/ /g);
+print(0 + @list);
